04138d345f7d301c3d37ed7aae557d0641bbc6b6
leejaejae <leesengjie@naver.com>
2025-01-23 21:33:19 +0900

    feat: 체크박스 기능 추가

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   addButton.addEventListener("click", () => {
     const todoTitle = prompt("할 일을 입력하세요.");
     if (todoTitle) {
-      const newTodo = { content: todoTitle, id: Date.now() };
+      const newTodo = { content: todoTitle, id: Date.now(), completed: false };
       todos.push(newTodo);
       localStorage.setItem("todos", JSON.stringify(todos));
 
@@ -22,11 +22,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function createTodoItem(todo) {
     const li = document.createElement("li");
-    li.textContent = todo.content;
-    li.className = "todo-item";
-    li.addEventListener("click", () => {
-      window.location.href = `/html/todo.html?id=${todo.id}`;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "checklists";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", (e) => {
+      todo.completed = e.target.checked;
+      li.classList.toggle("completed", e.target.checked);
+      localStorage.setItem("todos", JSON.stringify(todos));
     });
+
+    const label = document.createElement("label");
+    label.textContent = todo.content;
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.className = "todo-item";
+
+    // li.addEventListener("click", () => {
+    //   window.location.href = `/html/todo.html?id=${todo.id}`;
+    // });
+
     return li;
   }
+
+  const checkboxes = document.getElementsByName("checklists");
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", (e) => {
+      const li = e.target.parentNode;
+      const index = Array.from(checkboxes).indexOf(e.target);
+      todos[index].completed = e.target.checked;
+      li.classList.toggle("completed", e.target.checked);
+      localStorage.setItem("todos", JSON.stringify(todos));
+    });
+  });
 });
